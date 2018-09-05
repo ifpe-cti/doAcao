bec81fa93b23273a3e9cf10a90b0f7c6ec1bac36
milenasiqs <milena.ifpe@gmail.com>
2018-09-05 13:13:48 -0300

    com inicio das configuracoes do autocomplete

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-  export const FirebaseConfig = {
-    apiKey: "AIzaSyBiXo4LcyjgvBV9sQSH4MpoaegFJWk1B2c",
-    authDomain: "doacao-85d97.firebaseapp.com",
-    databaseURL: "https://doacao-85d97.firebaseio.com",
-    projectId: "doacao-85d97",
-    storageBucket: "",
-    messagingSenderId: "776860533609"
-  };  
