06d046843fdf37d9b5949454b02c0419f7c5b4cc
Joanne Gabriela <joanneifpe@gmail.com>
2018-11-20 17:15:53 -0300

    configurações do componente solicitacao-de-sangue

--- src/app/solicitacao-de-sangue/solicitacao-de-sangue.component.ts
+++ src/app/solicitacao-de-sangue/solicitacao-de-sangue.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {UsuariosService} from './../usuarios.service';
+import { Router} from '@angular/router';
+import { MenuItem } from 'primeng/primeng';
+import { MenusService } from '../menus.service';
+import { SolicitacaoSanguinea } from '../models/solicitacaoSanguinea';
+import { Usuario } from '../models/usuario';
+
 
 @Component({
   selector: 'app-solicitacao-de-sangue',
@@ -7,8 +14,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitacaoDeSangueComponent implements OnInit {
 
-  constructor() { }
+  cpf: String;
+  senha: String;
+  solicitacaoSanguinea: SolicitacaoSanguinea;
+  usuario: Usuario[];
 
+  constructor(private usuariosService: UsuariosService, 
+    private router:Router, private menusService: MenusService) { 
+      this.solicitacaoSanguinea = {
+       nomeUsuario: "", numeroDocumentoDoador: "", tipoSanguineo: ""
+
+      }
+    }
+/**
+ *  this.listarTodos()
+    .subscribe(meuObservable => 
+      this.todosOsUsuarios = meuObservable as Usuario[]
+    );
+ */
+   
+
+  buscarDados(){
+  if(this.usuariosService.usuarioLogado.cpf = this.cpf){
+    this.usuariosService.usuarioLogado.nome = this.solicitacaoSanguinea.nomeUsuario;
+    this.usuariosService.usuarioLogado.cpf = this.solicitacaoSanguinea.numeroDocumentoDoador;
+    this.usuariosService.usuarioLogado.tipoSanguineo = this.solicitacaoSanguinea.tipoSanguineo;
+  }  
+  
+ 
+}
   ngOnInit() {
   }
 
