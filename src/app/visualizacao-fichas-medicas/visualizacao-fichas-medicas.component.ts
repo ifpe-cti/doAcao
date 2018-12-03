2e72433f4720467af2eecd19249b8d1a2d7d116f
milenasiqs <milena.ifpe@gmail.com>
2018-12-03 09:49:32 -0300

    listagem exames (bug) + listagem fichas medicas

--- src/app/visualizacao-fichas-medicas/visualizacao-fichas-medicas.component.ts
+++ src/app/visualizacao-fichas-medicas/visualizacao-fichas-medicas.component.ts
@@ -3,6 +3,7 @@ import {FichasMedicasService} from '../fichas-medicas.service';
 import { Router} from '@angular/router';
 import { MenuItem } from 'primeng/primeng';
 import { MenusService } from '../menus.service';
+import { FichaMedica } from '../models/ficha-medica';
 
 
 @Component({
@@ -14,18 +15,79 @@ export class VisualizacaoFichasMedicasComponent implements OnInit {
  
   items: MenuItem[];
 
-  fichasMedicas: any[] = [];
-  fichaMedicaSelecionada;
+  fichaMedica: FichaMedica = {
+    idDoador: "", cpfDoador: "", hemoglobina: "",
+    pressaoArterial: "", temperatura: "", peso: "", altura: "", pulso: "", 
+    bracoPunsionado: "", reacoesAdversas: "", idFlebomista: "", nomeFlebomista: "", 
+    tipoDeDoacao: "", numeroDoTubo: "", volumeDoSangue: ""}; 
+
+  displayDialog: boolean;
+  selectedFicha: FichaMedica;
+  newFicha: boolean;
+  fichasMedicas: FichaMedica[] = [];
+  cols: any[];
 
   constructor(private fichasMedicasService: FichasMedicasService, 
     private router:Router, private menusService: MenusService) { }
 
-    voltarPaginaInicial(){
-      this.router.navigate(['dashboard-hemope']);
-     }
+    showDialogToAdd() {
+      this.newFicha = true;
+
+      this.fichaMedica = {
+        idDoador: "", cpfDoador: "", hemoglobina: "",
+        pressaoArterial: "", temperatura: "", peso: "", altura: "", pulso: "", 
+        bracoPunsionado: "", reacoesAdversas: "", idFlebomista: "", nomeFlebomista: "", 
+        tipoDeDoacao: "", numeroDoTubo: "", volumeDoSangue: ""}; 
+      }
+  
+  save() {
+      let fichas = [...this.fichasMedicas];
+      if (this.newFicha)
+          this.fichasMedicas.push(this.fichaMedica);
+      else
+          fichas[this.fichasMedicas.indexOf(this.selectedFicha)] = this.fichaMedica;
+  
+      this.fichasMedicas = fichas;
+      this.fichaMedica = null;
+      this.displayDialog = false;
+  }
+  
+  delete() {
+      let index = this.fichasMedicas.indexOf(this.selectedFicha);
+      this.fichasMedicas = this.fichasMedicas.filter((val, i) => i != index);
+      this.fichaMedica = null;
+      this.displayDialog = false;
+  
+      this.fichasMedicasService.apagarFichaMedicaFirebase(this.selectedFicha);
+  }
+  
+  onRowSelect(event) {
+    this.newFicha = false;
+    this.fichaMedica = this.cloneFichaMedica(event.data);
+    this.displayDialog = true;
+  }
+  
+  cloneFichaMedica(f: FichaMedica): FichaMedica {
+    let ficha = {
+      idDoador: "", cpfDoador: "", hemoglobina: "",
+      pressaoArterial: "", temperatura: "", peso: "", altura: "", pulso: "", 
+      bracoPunsionado: "", reacoesAdversas: "", idFlebomista: "", nomeFlebomista: "", 
+      tipoDeDoacao: "", numeroDoTubo: "", volumeDoSangue: ""}; 
+
+    for (let prop in f) {
+        ficha[prop] = f[prop];
+    }
+    return ficha;
+  }
+  
 
   ngOnInit() {
     this.items = this.menusService.itensHemope;
+
+    this.cols = [
+      { field: 'cpfDoador', header: 'CPF do Doador' },
+      { field: 'numeroDoTubo', header: 'Número do tubo' },
+    ];
 
     this.fichasMedicasService.listarTodos().subscribe(fichasMedicas => {
       this.fichasMedicas = fichasMedicas;
--- src/app/visualizacao-requisicao-exames/visualizacao-requisicao-exames.component.ts
+++ src/app/visualizacao-requisicao-exames/visualizacao-requisicao-exames.component.ts
@@ -3,6 +3,7 @@ import {RequisicaoDeExameService} from '../requisicao-de-exame.service';
 import { Router} from '@angular/router';
 import { MenuItem } from 'primeng/primeng';
 import {MenusService} from '../menus.service';
+import { RequisicaoDeExames } from '../models/requisicao-de-exames';
 
 @Component({
   selector: 'app-visualizacao-requisicao-exames',
@@ -12,23 +13,79 @@ import {MenusService} from '../menus.service';
 export class VisualizacaoRequisicaoExamesComponent implements OnInit {
 
   items: MenuItem[];
-  requisicaoExames: any[] = [];
-  requisicaoSelecionada;
+  exame: RequisicaoDeExames = {idDoador: "", numeroDocumentoDoador: "", 
+  tipoDocumentoDoador: "", orgaoExpeditorDoador: "", nomeMaeDoador:"", 
+  nomePaiDoador: "", tecnicoResponsavel: "", etiquetaDaAmostra: ""}; 
+
+  displayDialog: boolean;
+  selectedExame: RequisicaoDeExames;
+  newExame: boolean;
+  exames: RequisicaoDeExames[];
+  cols: any[];
+  
 
   constructor(private requisicaoExamesService: RequisicaoDeExameService, 
     private router:Router,  private menusSevice: MenusService) { }
 
-    voltarPaginaInicial(){
-      this.router.navigate(['dashboard-hemope']);
-     }
+    showDialogToAdd() {
+      this.newExame = true;
 
+      this.exame = {idDoador: "", numeroDocumentoDoador: "", 
+      tipoDocumentoDoador: "", orgaoExpeditorDoador: "", nomeMaeDoador:"", 
+      nomePaiDoador: "", tecnicoResponsavel: "", etiquetaDaAmostra: ""};
+      this.displayDialog = true;
+    }
+  
+  save() {
+      let exames = [...this.exames];
+      if (this.newExame)
+          exames.push(this.exame);
+      else
+          exames[this.exames.indexOf(this.selectedExame)] = this.exame;
+  
+      this.exames = exames;
+      this.exame = null;
+      this.displayDialog = false;
+  }
+  
+  delete() {
+      let index = this.exames.indexOf(this.selectedExame);
+      this.exames = this.exames.filter((val, i) => i != index);
+      this.exame = null;
+      this.displayDialog = false;
+  
+      this.requisicaoExamesService.apagarRequisicaoExamesFirebase(this.selectedExame);
+  }
+  
+  onRowSelect(event) {
+    this.newExame = false;
+    this.exame = this.cloneExame(event.data);
+    this.displayDialog = true;
+  }
+  
+  cloneExame(e: RequisicaoDeExames): RequisicaoDeExames {
+    let exame = {idDoador: "", numeroDocumentoDoador: "", 
+    tipoDocumentoDoador: "", orgaoExpeditorDoador: "", nomeMaeDoador:"", 
+    nomePaiDoador: "", tecnicoResponsavel: "", etiquetaDaAmostra: ""};
 
-
+    for (let prop in e) {
+        exame[prop] = e[prop];
+    }
+    return exame;
+  
+  }
+  
+ 
   ngOnInit() { 
       this.items = this.menusSevice.itensHemope;
  
+      this.cols = [
+        { field: 'numeroDocumentoDoador', header: 'Número do documento do doador' },
+        { field: 'etiquetaAmostra', header: 'Etiqueta da amostra' },
+      ];
+
     this.requisicaoExamesService.listarTodos().subscribe(requisicaoExames => {
-      this.requisicaoExames = requisicaoExames;
+      this.exames = requisicaoExames;
     });
   }
 
