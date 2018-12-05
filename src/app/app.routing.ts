4f7e4fdc477cdcf70125babb561cf9fe378c2133
milenasiqs <milena.ifpe@gmail.com>
2018-12-05 09:45:28 -0300

    erro do banco ajeitado

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,7 +7,6 @@ import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { InicialComponent } from './inicial/inicial.component';
 import { RequisicaoDoExameComponent } from './requisicao-do-exame/requisicao-do-exame.component';
-import { BancoDeSangueComponent } from './banco-de-sangue/banco-de-sangue.component';
 import { FichasMedicasComponent } from './fichas-medicas/fichas-medicas.component';
 import { AgendamentoDeDoacoesComponent } from './agendamento-de-doacoes/agendamento-de-doacoes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -51,7 +50,6 @@ const APP_ROUTES: Routes = [
 { path: 'login', component: LoginComponent },
 { path: 'cadastro', component: CadastroComponent }, 
 { path: 'requisicao-do-exame', component: RequisicaoDoExameComponent },
-{ path: 'banco-de-sangue', component: BancoDeSangueComponent },
 { path: 'fichas-medicas', component: FichasMedicasComponent },
 { path: 'agendamento-de-doacoes', component: AgendamentoDeDoacoesComponent },
 { path: 'login-google', component: LoginGoogleComponent },
