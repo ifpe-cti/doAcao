d6e08c3474d42d1407d64f7d0c1697aa98ada4b7
milenasiqs <milena.ifpe@gmail.com>
2018-12-06 17:07:11 -0300

    tipos sanguíneos hemope atualizados

--- src/app/maisInformacoesHemope/tipos-sanguineos-hemope/tipos-sanguineos-hemope.component.ts
+++ src/app/maisInformacoesHemope/tipos-sanguineos-hemope/tipos-sanguineos-hemope.component.ts
@@ -10,9 +10,39 @@ import { MenusService } from '../../menus.service';
 })
 export class TiposSanguineosHemopeComponent implements OnInit {
 
-  constructor(private menusSevice: MenusService, private router:Router) { }
-
+  data: any;
   items: MenuItem[];
+
+  constructor(private menusSevice: MenusService, private router:Router) { 
+    this.items = this.menusSevice.itensMaisInformacoes;
+    this.data = {
+      labels: ['A+','B+','O+','AB+','A-','B-','O-','AB-'],
+      datasets: [
+          {
+              data: [36,9,37,3,6,2,7,1],
+              backgroundColor: [
+                "#FF0040",
+                "#FE2E64",
+                "#FA5882",
+                "#F7819F",
+                "#F5A9BC",
+                "#F6CED8",
+                "#FE2E9A",
+                "#DF0174",
+              ],
+              hoverBackgroundColor: [
+                "#FF0040",
+                "#FE2E64",
+                "#FA5882",
+                "#F7819F",
+                "#F5A9BC",
+                "#F6CED8",
+                "#FE2E9A",
+                "#DF0174",
+              ]
+          }]    
+      };
+  }
 
   ngOnInit() {
     this.items = this.menusSevice.itensMaisInformacoesHemope;
