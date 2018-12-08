bc22f1fa778c1a16ff26ed89ad6f3f84d2e0975e
Joanne Gabriela <joanneifpe@gmail.com>
2018-12-07 22:45:12 -0300

    testes models: usuario — issue #88

--- src/app/models/usuario.spec.ts
+++ src/app/models/usuario.spec.ts
@@ -1,10 +1,29 @@
 import { Usuario } from "./usuario";
 
-describe("Conjunto de testes do Usuário", ()=>{
-    it("Deve retornar falso, pois é um usuário inválido", ()=>{
-        let usuario:Usuario = new Usuario();
-
+describe("Conjunto de testes do Usuário", () => {
+    it("Deve retornar falso, pois é um usuário inválido", () => {
+        let usuario: Usuario = new Usuario();
         expect(usuario.validarUsuario()).toBeFalsy();
+    })
+
+    it("Deve retornar verdadeiro, pois todos os dados do usuário sá válidos", () => {
+        let usuario: Usuario = new Usuario();
+        
+        this.usuario.nome = "Kim Minseok"
+        this.usuario.user = "xiumin"
+        this.usuario.senha = "xiumin"
+        this.usuario.dataNascimento = "25/03/1190"
+        this.usuario.tipoSanguineo = "A+"
+        this.usuario.tipoUsuario = "usuario"
+        this.usuario.cpf = "123.456.789-10"
+        this.usuario.nomePai = "Pai"
+        this.usuario.nomeMae = "Mãe"
+        this.usuario.numeroDocumento = "186523-5"
+        this.usuario.tipoDocuemento = "Carteira de Reservista"
+        this.usuario.orgaoExpeditorDocuemento = "SSA"
+
+        expect(usuario.validarUsuario()).toBeTruthy();
+
     })
 }
 
