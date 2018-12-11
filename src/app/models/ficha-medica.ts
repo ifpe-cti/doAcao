f5e963c0e3728160a46615d0c994d5b17f0cbfd3
Joanne Gabriela <joanneifpe@gmail.com>
2018-12-11 00:06:34 -0300

    pulso e peso ok em fichas médicas #119

--- src/app/models/ficha-medica.ts
+++ src/app/models/ficha-medica.ts
@@ -21,29 +21,32 @@ export class FichaMedica {
     validarFichaMedica() {
         if (this.idDoador == "" || this.id == "" || this.cpfDoador == "" || this.hemoglobina == "" || this.pressaoArterial == "" ||
             this.temperatura == "" || this.peso == "" || this.altura == "" || this.bracoPunsionado == "" || this.reacoesAdversas == "" ||
-            this.nomeFlebomista == "" || this.tipoDeDoacao == "" || this.numeroDoTubo == "" 
-            || this.volumeDoSangue == "") {
-                return false
+            this.nomeFlebomista == "" || this.tipoDeDoacao == "" || this.numeroDoTubo == ""
+            || this.volumeDoSangue == "" || this.pulso == "" || this.peso == "") {
+            return false
         }
 
         return true
     }
 
-    toDocument(){
-        return{
+    toDocument() {
+        return {
             idDoador: this.idDoador,
             cpfDoador: this.cpfDoador,
             hemoglobina: this.hemoglobina,
             pressaoArterial: this.pressaoArterial,
-            temperatura: this.temperatura, 
+            temperatura: this.temperatura,
             preso: this.peso,
             altura: this.altura,
-            bracoPunsionado: this.bracoPunsionado, 
-            reacoesAdversas: this.reacoesAdversas, 
+            bracoPunsionado: this.bracoPunsionado,
+            reacoesAdversas: this.reacoesAdversas,
             nomeFlebomista: this.nomeFlebomista,
             tipoDeDoacao: this.tipoDeDoacao,
             numeroDoTubo: this.numeroDoTubo,
-            volumeDoSangue: this.volumeDoSangue
+            volumeDoSangue: this.volumeDoSangue,
+            peso: this.peso,
+            pulso: this.pulso
+
         }
     }
 
